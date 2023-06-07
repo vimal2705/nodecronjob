fc2ac4126103dd8a5774afaf32278cbb7f7c2648
vimal <vimalnavadiya2705@gmail.com>
2023-06-07 09:54:01 +0530

    wed first

--- index.js
+++ index.js
@@ -14,31 +14,27 @@ app.get("/", (req, res) => {
 
 app.get("/notify", async (req, res) => {
   const message = {
-    topic: "MyNews",
+
     notification: {
       title: "Test Notification",
       body: "Here is the Notification Description",
     },
     data: {
-      name: "AboutReact",
-      url: "https://aboutreact.com",
-      writter: "Snehal Agrawal",
+        "score": "5x1",
+        "time": "10:15"
     },
     android: {
       notification: {
         image:
           "https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png",
       },
-      ttl: 4500,
-      priority: "normal",
+      ttl: 36000 * 10000,
+      priority: "high",
     },
-    apns: {
-      headers: {
-        "apns-priority": "5",
-        "apns-expiration": "1604750400",
-      },
-    },
+    
   };
+//   const alldata =   admin.firestore().getAll
+
 admin.messaging()
     .sendToDevice("fb3yI1CBR1umrt0TqJcUuV:APA91bHc5ZLAR3kmzC8kAD2PJrDD5bZvmfZYrxxT1WD_ZqnQnTFASLAIDAYyxxZSXJu1IGzJXb2NzUSeqoD7OOjIL7-1brSTtFoPRcivknjSCYd_o0bMAWHNN5pzJQbTTd9PmYA2nJ7C", message)
     .then((response) => {
